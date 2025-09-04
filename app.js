7bb5df02a3b445f5c0af4de0bf60979334d18887
Paula Béguelin <beguelinpn@gmail.com>
2025-09-04 17:43:07 -0300

    mostrarLista

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+
+let amigos = []//arreglo vacío
 
 function agregarAmigo(){ 
     let input = document.getElementById("amigo");
@@ -12,3 +13,14 @@ function agregarAmigo(){
     mostrarLista();
     input.value = ""; // limpiar campo
 }
+
+function mostrarLista() {
+      let lista = document.getElementById("listaAmigos");
+      lista.innerHTML = "";// Vacía la lista
+
+      amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+      });
+    }
